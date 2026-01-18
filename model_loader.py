ea5bdf132d475cd880c66a3ad901c30a50de0cfb
Susmita Sahu <161750185+SusmitaSahu365@users.noreply.github.com>
2026-01-18 15:02:44 +0530

    Refactor model_loader by removing unused code
    Removed model loading and downloading logic.

--- model_loader.py
+++ model_loader.py
@@ -1,11 +1,1 @@
-import os
-import gdown
-from keras.models import load_model
 
-MODEL_PATH = "model/Modelmain.h5"
-MODEL_URL = "https://drive.google.com/file/d/1fBQdulES8JMWcm-SDbCxvkHZ58pLGqU_/view?usp=drive_link"
-
-if not os.path.exists(MODEL_PATH):
-    gdown.download(MODEL_URL, MODEL_PATH, quiet=False)
-
-model = load_model(MODEL_PATH)
